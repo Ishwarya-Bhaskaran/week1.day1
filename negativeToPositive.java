c23eada7f4f85dc135e5fb79f3ece7ec005e3c0f
Ishwarya-Bhaskaran <92179044+Ishwarya-Bhaskaran@users.noreply.github.com>
2021-10-08 23:51:49 +0530

    Add files via upload

---
+++ negativeToPositive.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class negativeToPositive {
+	public static void main (String[] args)
+	{
+		 int i=-60;
+		 if (i<0)
+		 {System.out.println("The number -60 is converted to 60");}
+		 
+		 else 
+		 {System.out.println("Positive number");}
+	}
+
+}
